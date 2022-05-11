4042fca2d1667f14151904a03c08778a8f706f84
Vladislav Rishe <vladr08@gmail.com>
2022-05-11 09:47:46 +0300

    Remove 'close_all_tabs' command.
    ST3 has its own, named 'close_all'.

--- close_all_tabs.py
+++
@@ -1,7 +0,0 @@
-import sublime, sublime_plugin
-
-class CloseAllTabsCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        for view in self.window.views():
-            self.window.focus_view(view)
-            self.window.run_command("close_file")
